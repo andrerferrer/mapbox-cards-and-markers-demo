8eaf6ba97ee736eb4206e2254560b0f58ca74114
andre ferrer <andrerferrer@hotmail.com>
2020-11-30 20:04:04 -0300

    make cards clickable (JS)

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,5 @@
 import mapboxgl from 'mapbox-gl';
 
-// add this function
 const zoomMapToMarker = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach((marker) => {
@@ -9,18 +8,58 @@ const zoomMapToMarker = (map, markers) => {
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
 
+const restaurantClickHandler = (map, mapboxMarker) => {
+  return (event) => {
+    // https://docs.mapbox.com/mapbox-gl-js/api/markers/#marker#togglepopup
+    mapboxMarker.togglePopup();
+    // this was taken from: https://docs.mapbox.com/mapbox-gl-js/example/flyto/
+    map.flyTo({
+      // https://docs.mapbox.com/mapbox-gl-js/api/markers/#marker#setlnglat
+      center: mapboxMarker.getLngLat(),
+      essential: true
+    });
+  }
+}
+
+const makeRestaurantCardClickable = (markerData, mapboxMarker, map) => {
+  // We'll add an id to the marker (the same as the restaurant). This is coming from the controller.
+  const markerDiv = mapboxMarker.getElement();
+  markerDiv.id = `marker-${markerData.id}`;
+  // Then, we need to find the card that we will click on
+  const restaurantCard = document.getElementById(`restaurant-card-${markerData.id}`);
+
+  // On that card, we will add some behavior if a click happens
+  restaurantCard.addEventListener('click', restaurantClickHandler(map, mapboxMarker));
+  // WARNING: if the event listener callback is arcane to you, check the alternative code below
+
+  // we can write this event listener here like this
+  // restaurantCard.addEventListener('click', (event) => {
+  //   // https://docs.mapbox.com/mapbox-gl-js/api/markers/#marker#togglepopup
+  //   mapboxMarker.togglePopup();
+  //   // this was taken from: https://docs.mapbox.com/mapbox-gl-js/example/flyto/
+  //   map.flyTo({
+  //     // https://docs.mapbox.com/mapbox-gl-js/api/markers/#marker#setlnglat
+  //     center: mapboxMarker.getLngLat(),
+  //     essential: true
+  //   });
+  // })
+}
+
 const addMarkersToMap = (map) => {
   const mapElement = document.getElementById('map');
 
-  // Take the marker from the view in `app/views/restaurants/show.html.erb`
   const markers = JSON.parse(mapElement.dataset.markers);
-  markers.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.popUp);
+  
+  markers.forEach((markerData) => {
+    const popup = new mapboxgl.Popup().setHTML(markerData.popUp);
 
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
+    const mapboxMarker = new mapboxgl.Marker()
+      .setLngLat([ markerData.lng, markerData.lat ])
       .setPopup(popup)
       .addTo(map);
+
+    makeRestaurantCardClickable(markerData, mapboxMarker, map);
+
   });
   
   zoomMapToMarker(map, markers)
@@ -30,10 +69,7 @@ const addMarkersToMap = (map) => {
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  // This if here is to ensure that we will only run the code
-  // IF we find this map element (which will be only in the Restaurants#Show)
   if (mapElement) {
-    // This is setting the access token as the one coming from app/views/restaurants/show.html.erb (line 11)
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
